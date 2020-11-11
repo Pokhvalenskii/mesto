9b646c97cf5b9f8ce7e5206af062e52e3eec21a9
pokhvalenskii <pokhvalenskii@gmail.com>
2020-11-11 17:43:05 +0300

    feat: card opening

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
 const popup = document.querySelector('.popup');
 const btnEdit = document.querySelector('.profile__btn-edit');
 const btnClose = document.querySelector('.popup__btn-close-image');
-const form = document.querySelector('.popup__form')
+const btnCloseImg = document.querySelector('.popup-img__btn-close-image');
 
+const form = document.querySelector('.popup__form');
 const btnAdd = document.querySelector('.profile__btn-add');
-
 const name = document.querySelector('.profile__person-name');
 const status = document.querySelector('.profile__person-status');
 const nameValue = document.querySelector('.popup__input-line_type_name');
 const statusValue = document.querySelector('.popup__input-line_type_status');
-
 const arrayCards = [
   {
     name: 'Архыз',
@@ -37,10 +36,33 @@ const arrayCards = [
   }
 ];
 
+const popupImage = document.querySelector('.popup-img');
+
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+btnEdit.addEventListener('click', showPopup);
+btnClose.addEventListener('click', showPopup);
+form.addEventListener('submit', saveEdits);
+btnAdd.addEventListener('click', showPopup);
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+const cardTemplate = document.querySelector('#tempCard').content;
+const cards = document.querySelector('.cards');
+const showDefault = arrayCards.map(function(item){
+  const card = cardTemplate.cloneNode(true);
+  const cardText = card.querySelector('.card__text');
+  const cardImage = card.querySelector('.card__image');
+  cardText.textContent = item.name;
+  cardImage.src = item.link;
+  cards.append(card);
+});
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
 function showPopup (Event) {
   const evtTarget = Event.target;
-  // console.log(evtTarget);
 
   if(evtTarget === btnAdd) {
     popup.classList.add('popup_active');
@@ -84,45 +106,51 @@ function showPopup (Event) {
       status.textContent = statusValue.value;
     }
   }
-};
+
+  if(evtTarget === cardImage){
+    console.log('жмякнул картинку');
+    console.log(evtTarget);
+    popupImage.classList.add('popup-img_active')
+    popupImage.querySelector('.popup-img__image').src = cardImage.src;
+  }
+
+  if(evtTarget === btnCloseImg) {
+    console.log('закрыть')
+    popupImage.classList.remove('popup-img_active');
+  }
+}
 
 function addCard (name, link) {
-
   if((name && link) !== '') {
     const card = cardTemplate.cloneNode(true);
     const cardText = card.querySelector('.card__text');
     const cardImage = card.querySelector('.card__image');
     cardText.textContent = name;
     cardImage.src = link;
-    cards.append(card);
+    cards.prepend(card);
   } else {
     alert('Незьзя добавлять пустые поля');
   }
-  // console.log(cardText, ' | ', cardImage);
+}
+
+function removeCard (e){
+  console.log('Удаляем')
 }
 
 function saveEdits (e) {
   e.preventDefault();
   showPopup(e);
-};
-
-btnEdit.addEventListener('click', showPopup);
-btnClose.addEventListener('click', showPopup);
-form.addEventListener('submit', saveEdits);
-btnAdd.addEventListener('click', showPopup);
+}
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
-const cardTemplate = document.querySelector('#tempCard').content;
-const cards = document.querySelector('.cards');
+const btnRemove = document.querySelector('.card__btn-remove');
+console.log(btnRemove);
 
-const showDefault = arrayCards.map(function(item){
-  const card = cardTemplate.cloneNode(true);
-  const cardText = card.querySelector('.card__text');
-  const cardImage = card.querySelector('.card__image');
-  cardText.textContent = item.name;
-  cardImage.src = item.link;
-  cards.append(card);
+const cardImage = document.querySelector('.card__image');
+cardImage.addEventListener('click', showPopup);
+btnCloseImg.addEventListener('click', showPopup);
+
+btnRemove.addEventListener('click', (Event) => {
+  console.log(Event);
 });
-
-// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
