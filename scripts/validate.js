502dc1f33f827c9540d0a55c3f8c0ea47abc4b47
pokhvalenskii <pokhvalenskii@gmail.com>
2020-11-25 02:14:15 +0300

    hotfix: validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -8,22 +8,39 @@ function enableValidation () {
   formList.forEach((formElement) => {
       formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
-      console.log('нажал на форму')
-      console.log(formElement)
+      // console.log('нажал на форму')
+      // console.log(formElement)
     });
     
     const inputList = formElement.querySelectorAll('.popup__input');
-    //console.log('input list: ', inputList)
+    console.log('input list: ', inputList)
     
+    const submit = formElement.querySelector('.popup__btn-save');
+    console.log(submit)
+    //submitCheck (submit, inputList);
     inputList.forEach((inputElement) => {
       //console.log(inputElement);
       inputElement.addEventListener('input', () => {
         //console.log('inputEvent: ', inputElement.validity.valid);
-        checkValidity(inputElement, formElement);
+        checkValid(inputElement, formElement);
+        //console.log('НАША ФОРМА', formElement.checkValidity());
+        submitCheck(formElement, submit);
       })
     })
-
   });
+}
+
+function submitCheck (formElement, submit) {
+  if(formElement.checkValidity()){
+    console.log('Наша форма валидна');
+    submit.disabled = false;
+    //formElement.classList.add('checked-valid');
+  } else {
+    console.log('наша форма не валидна');
+    submit.disabled = true;
+    //formElement.classList.add('checked-IN-valid');
+
+  }
 }
 
 function showError (inputElement, formElement) {
@@ -38,7 +55,9 @@ function hideError (inputElement, formElement) {
   inputElement.classList.remove('popup__input_status_invalid');
 }
 
-function checkValidity (inputElement, formElement) {
+
+
+function checkValid (inputElement, formElement) {
  if(inputElement.validity.valid) {
    console.log('valid element: ', inputElement);
    hideError(inputElement, formElement)
